b122c6cbf1ce8a21209c3f29bae00799bb9a64df
Emrikcah <curryandr3@gmail.com>
2021-11-11 05:32:41 -0500

    initial setup

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
-export default function Home() {
+
+export function Home() {
   return (
     <div className={styles.container}>
       <Head>
@@ -65,5 +66,8 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
+
+
+
