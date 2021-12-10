09f43d55d3e9560824c97a239fafb41fe38fb6ac
Emrikcah <curryandr3@gmail.com>
2021-12-10 08:07:26 -0500

    deleted unused prop in button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,2 +1,2 @@
 //This button is more dynamic now being a component
-export const Button = ({children,buttonStyle,onClick}) => <button onClick={onClick} className="btn">{children}</button>;
+export const Button = ({children,onClick}) => <button onClick={onClick} className="btn">{children}</button>;
